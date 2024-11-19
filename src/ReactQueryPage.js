94d8f96d3880de73e15c6311260a6bd714081049
devdong9897 <dev.dong9897@gmail.com>
2024-11-19 14:56:32 +0900

    디테일데이터 불러오기

--- src/ReactQueryPage.js
+++ src/ReactQueryPage.js
@@ -11,22 +11,23 @@ const ReactQueryPage = () => {
   // 데이터가 로딩 중일 때 로딩 상태를 관리하고, 데이터가 성공적으로 로드되면
   // 그 데이터를 저장하고 화면에 표시하는 작업을 자동으로 처리
 
-  const fetchPost = () => {
-    return axios.get("http://localhost:3004/posts");
+  const fetchPost = (queryData) => {
+    const id = queryData.queryKey[1];
+    return axios.get(`http://localhost:3004/posts/${id}`);
   };
   // api호출을 통해 받은 데이터
-  const { isLoading, data, isError, error } = useQuery({
-    queryKey: ["posts"], // 데이터 요청에 대한 고유한 식별자
+  const { isLoading, data, isError, error, refetch } = useQuery({
+    queryKey: ["posts", 1], // 데이터 요청에 대한 고유한 식별자
     queryFn: fetchPost, // 내가 호출하고 싶은 api ( 데이터를 가져올 함수)
     retry: 1,
-    staleTime: 60000, // 데이터를 새로 요청하지 않고 캐시된 데이터를 계속 사용할지를 정의하는 것. // 자주 호출할 필요가 없는 api
+    // staleTime: 60000, // 데이터를 새로 요청하지 않고 캐시된 데이터를 계속 사용할지를 정의하는 것. // 자주 호출할 필요가 없는 api
 
-    // 캐쉬 수명조절... 캐쉬도 메모리공간을 차지하기 때문에 주기적으로 비워줄 필요가있다.
-    gcTime: 10000, // 설정하지 않으면 기본값이 5분 // staleTime < gcTime
+    // // 캐쉬 수명조절... 캐쉬도 메모리공간을 차지하기 때문에 주기적으로 비워줄 필요가있다.
+    // gcTime: 10000, // 설정하지 않으면 기본값이 5분 // staleTime < gcTime
     select: (data) => {
       return data.data;
     },
-    refetchOnWindowFocus: true,
+    enabled: false,
   });
   console.log("ddd", data, isLoading);
   console.log("error", isError, error);
@@ -40,9 +41,10 @@ const ReactQueryPage = () => {
   }
   return (
     <div>
-      {data.map((item) => (
+      {/* {data?.map((item) => (
         <div>{item.title}</div>
-      ))}
+      ))} */}
+      <button onClick={refetch}>post리스트 다시 들고오기</button>
     </div>
   );
 };
